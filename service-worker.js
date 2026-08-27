84674e9fb693715af69bf04eb6a8a66e7f6eef5d
Ursula030 <amor9420@gmail.com>
2026-08-27 10:12:20 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='stock-radar-v5-8';
+const CACHE='stock-radar-v5-19-3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./app-icon-192.png','./app-icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
